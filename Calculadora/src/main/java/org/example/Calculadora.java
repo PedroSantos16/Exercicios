1ecdd6a9f68e0dcca38a44cf1f45c980fb9b9bf0
Pedro H <henri.pedrosantos1310@gmail.com>
2026-05-11 20:36:07 -0300

    calculos basicos

---
+++ Calculadora/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Escolha entre 'basicas' ou 'sair': ");
+            String entrada = scanner.nextLine();
+
+            if (entrada.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            if (entrada.toLowerCase().startsWith("media")) {
+                String[] parts = entrada.trim().split("\\s+");
+                if (parts.length < 2) {
+                    System.out.println("Uso Correto deve ser: media 1 2 3");
+                    continue;
+                }
+
+                double soma = 0;
+                boolean erro = false;
+
+                for (int i = 1; i < parts.length; i++) {
+                    soma += Double.parseDouble(parts[i]);
+                    System.out.println("Valor invalido: " +parts[i]);
+                    erro = true;
+                    break;
+                }
+                 if (erro) {
+                     continue;
+                 }
+
+                 double media = soma / (parts.length - 1);
+                 System.out.println("Media aritimetica: " + media);
+                 continue;
+            }
+
+            if (entrada.toLowerCase().startsWith("basicas")) {
+                String[] parts = entrada.trim().split("\\s+");
+                if (parts.length != 4) {
+                    System.out.println("Uso correto deve ser: basicas 10 + 5");
+                    continue;
+                }
+
+                double a;
+                double b;
+
+                try {
+                    a = Double.parseDouble(parts[1]);
+                    b = Double.parseDouble(parts[3]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Digite apenas numeros validos");
+                    continue;
+                }
+
+                String operator = parts[2];
+                double resultado = 0;
+
+                switch (operator) {
+                    case "+":
+                        resultado = a + b;
+                        break;
+                    case "-":
+                        resultado = a - b;
+                        break;
+                    case "*":
+                        resultado = a * b;
+                        break;
+                    case "/":
+                        if (b == 0) {
+                            System.out.println("Erro: divisão por zero não pode ser realizada.");
+                        }
+                        resultado = a / b;
+                        break;
+                    default:
+                        System.out.println("Operador invalido.");
+                        continue;
+                }
+                System.out.println("Resultado: " + resultado);
+                continue;
+            }
+
+            scanner.close();
+        }
+    }
+}
